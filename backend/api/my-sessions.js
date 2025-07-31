8661f8b079db21e19d4b5df8e3c91c7727d391ff
preetisharma383 <sharma383preeti@gmail.com>
2025-07-31 08:02:25 +0530

    intial commit

--- backend/api/my-sessions.js
+++
@@ -1,6 +0,0 @@
-const Session=require("../models/Session");
-
-const Session = await Session.find({
-  createdBy: req.user.id,
-  status: { $ne: 'deleted' } // 👈 Only show non-deleted
-});
--- server/server.js
+++ server/server.js
@@ -3,9 +3,11 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
 
+
 const app = express();
 app.use(cors());
 app.use(express.json());
+
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
@@ -19,3 +21,14 @@ app.use('/api', sessionRoutes);
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
+const path = require('path');
+
+// Serve React build files in production
+if (process.env.NODE_ENV === 'production') {
+  const clientPath = path.resolve(__dirname, '../client/dist');
+  app.use(express.static(clientPath));
+
+  app.get(/^\/(?!api).*/, (req, res) => {
+  res.sendFile(path.join(clientPath, 'index.html'));
+});
+}
